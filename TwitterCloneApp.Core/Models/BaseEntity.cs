bacb5f5a9f26d03b3c4d59b8175830cb12c25b50
onatege <ege.karaman@bilgiedu.net>
2023-07-17 17:28:53 +0300

    Added Base and User Entities.

---
+++ TwitterCloneApp.Core/Models/BaseEntity.cs
@@ -0,0 +1,9 @@
+﻿namespace TwitterCloneApp.Core.Models
+{
+    public class BaseEntity
+    {
+        public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+    }
+}
---
+++ TwitterCloneApp.Core/Models/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TwitterCloneApp.Core.Models
+{
+    public class User : BaseEntity
+    {
+        public string UserName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool isDeleted { get; set; }
+        public string Biography { get; set; }
+        public string ProfileImg { get; set; }
+        public DateTime DeletedAt { get; set; }
+    }
+}
