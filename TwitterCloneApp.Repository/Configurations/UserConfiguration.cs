7a5a75f999e37c0357dc89d7c403e0a5cf046864
Canberk Coşkun <121981226+canberkcoskun1@users.noreply.github.com>
2023-07-18 12:22:21 +0300

    Configuration settings have been made for the User entity.

---
+++ TwitterCloneApp.Repository/Configurations/UserConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterCloneApp.Core.Models;
+
+namespace TwitterCloneApp.Repository.Configurations
+{
+	public class UserConfiguration : IEntityTypeConfiguration<User>
+	{
+		public void Configure(EntityTypeBuilder<User> builder)
+		{
+			builder.HasKey(x => x.Id);
+			builder.Property(x => x.Id).UseIdentityColumn();
+			builder.Property(x => x.UserName).IsRequired().HasMaxLength(50);
+			builder.Property(x => x.Email).IsRequired().HasMaxLength(50);
+			builder.Property(x => x.Password).IsRequired().HasMaxLength(20);
+			builder.Property(x => x.Biography).HasMaxLength(30);
+			builder.ToTable("Users");
+		}
+	}
+}
