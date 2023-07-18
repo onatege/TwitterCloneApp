4c27001b72622778efc16c609cb1b9cb62ae982f
Canberk Coşkun <121981226+canberkcoskun1@users.noreply.github.com>
2023-07-18 15:02:01 +0300

    Follow,Like and TwReplyConfiguration defined.

---
+++ TwitterCloneApp.Repository/Configurations/LikeConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterCloneApp.Core.Models;
+
+namespace TwitterCloneApp.Repository.Configurations
+{
+	public class LikeConfiguration : IEntityTypeConfiguration<Like>
+	{
+		public void Configure(EntityTypeBuilder<Like> builder)
+		{
+			builder.HasKey(x => new { x.UserId, x.TweetId });
+			builder.ToTable("Likes");
+		}
+	}
+}
---
+++ TwitterCloneApp.Repository/Configurations/TwReplyConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitterCloneApp.Core.Models;
+
+namespace TwitterCloneApp.Repository.Configurations
+{
+	public class TwReplyConfiguration : IEntityTypeConfiguration<TwReply>
+	{
+		public void Configure(EntityTypeBuilder<TwReply> builder)
+		{
+			builder.HasKey(tw => new { tw.TweetId, tw.ReplyId });
+			builder.ToTable("Replies");
+
+		}
+	}
+}
