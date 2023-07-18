f29ad62044addd9c2e4a84ee8c0740530b90637e
onatege <ege.karaman@bilgiedu.net>
2023-07-18 15:08:55 +0300

    AppDbContext configurations adjusted.

--- TwitterCloneApp.Repository/AppDbContext.cs
+++ TwitterCloneApp.Repository/AppDbContext.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using TwitterCloneApp.Core.Models;
 
 namespace TwitterCloneApp.Repository
 {
-    internal class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        { 
+        }
+        public DbSet<User> Users { get; set; }
+        public DbSet<Tweet> Tweets { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Follow> Follows { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<TwReply> TwReplies { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
