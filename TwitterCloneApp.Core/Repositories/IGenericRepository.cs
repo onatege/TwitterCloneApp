4d76e0f5e084134d978e034877f771b17fa99dda
onatege <ege.karaman@bilgiedu.net>
2023-07-18 12:31:46 +0300

    Generic Repository interface added (IGenericRepository)

---
+++ TwitterCloneApp.Core/Repositories/IGenericRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TwitterCloneApp.Core.Repositories
+{
+    internal interface Interface1
+    {
+    }
+}
