b3973ebaa6e5a0a6f3e71bf79ea64a782fa3d084
Canberk Coşkun <121981226+canberkcoskun1@users.noreply.github.com>
2023-07-21 09:34:30 +0300

    Sending data to db with context has been tested in the TagController.

---
+++ TwitterCloneApp/Controllers/TagController.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TwitterCloneApp.Core.Models;
+using TwitterCloneApp.Repository;
+
+namespace TwitterCloneApp.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class TagController : ControllerBase
+	{
+		private readonly AppDbContext _dbContext;
+		public TagController(AppDbContext dbContext)
+		{
+			_dbContext = dbContext;
+		}
+
+		[HttpGet]
+		public async Task<IActionResult> GetAllTags()
+		{
+			var tweets = await _dbContext.Tags.ToListAsync();
+			return Ok(tweets);
+		}
+		[HttpGet("{id}")]
+		public async Task<IActionResult> GetTagsById(int id)
+		{
+			var tag = await _dbContext.Tags.FindAsync(id);
+			return Ok(tag);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> NewTag()
+		{
+			var tag = new Tag
+			{
+				Name = "#testing",
+				isTrending = true,
+			};
+	
+			await _dbContext.Tags.AddAsync(tag);
+			await _dbContext.SaveChangesAsync();
+			return Ok(tag);
+		}
+
+		[HttpDelete("{id}")]
+		public async Task<IActionResult> RemoveTag(int id)
+		{
+			var tag = _dbContext.Tags.Find(id);
+
+			if(tag is null) { return NotFound(); };
+
+			_dbContext.Tags.Remove(tag);
+			await _dbContext.SaveChangesAsync();
+			return Ok(tag);
+		}
+	}
+}
